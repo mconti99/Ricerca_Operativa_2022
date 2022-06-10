e41121b1b32a9d84663dfbfd196ebae038752005
francescoiannaccone <francescoiannaccone@live.it>
2022-06-10 19:03:13 +0200

    v4

--- benchmark/create_benchmarks.py
+++ benchmark/create_benchmarks.py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 
-n_points = [500,100,200,500,1400,1000,400,700,300,800,5000,10000]
-n_clusters = [7,4,3,2,3,2,4,5,4,6,4,5,10]
-dim_points = [32,16,24,10,20,16,12,20,64,32,3,3]
+n_points = [500,1000,1000,1000,1000,1000,1500,1500,2000,3000,5000,10000]
+n_clusters = [5,2,4,5,6,7,5,10,5,5,5,10]
+dim_points = [32,32,32,32,20,20,18,18,18,16,16,16]
 
 for i in range(12):
     points, _ = make_blobs(n_samples=n_points[i], centers=n_clusters[i], n_features=dim_points[i], random_state=0)
